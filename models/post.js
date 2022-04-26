f6386ac6a21afbc98014b4a05a98e5d9874c927c
Merissa Bridgeman <merissabridgeman@Merissas-MacBook-Pro-2.local>
2022-04-26 11:51:13 -0700

    added auth tests

--- models/post.js
+++ models/post.js
@@ -1,12 +1,21 @@
-const {Schema, model} = require('mongoose');
+const { Schema, model } = require('mongoose');
+const Populate = require('../util/autopopulate');
 
 const postSchema = new Schema({
-    title: {type: String, required: true},
-    url: {type: String, required: true},
-    summary: {type: String, required: true},
-    subreddit: { type: String, required: true },
-    comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+  title: { type: String, required: true },
+  url: { type: String, required: true },
+  summary: { type: String, required: true },
+  comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+  upVotes: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  downVotes: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  voteScore: { type: Number },
+  author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  subreddit: { type: String, required: true }
 });
 
+// Always populate the author field
+postSchema
+  .pre('findOne', Populate('author'))
+  .pre('find', Populate('author'));
 
 module.exports = model('Post', postSchema);
